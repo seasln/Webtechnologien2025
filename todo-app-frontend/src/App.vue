ab298a4c58ee9acdc39ee1b4db5333f0faa3889d
nicolenatalie2203 <Nicole-Natalie.Rudloff@Student.HTW-Berlin.de>
2025-10-22 15:50:39 +0200

    v-for Schleife für Liste erstellt

--- todo-app-frontend/src/App.vue
+++ todo-app-frontend/src/App.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+let list = ['String 1', 'String 2', 'String 3'];
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +8,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <div v-for="string of list">{{string}}</div>
 </template>
 
 <style scoped></style>

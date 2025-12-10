82554b8a710c287a0d16097f3d76588742487a96
nicolenatalie2203 <Nicole-Natalie.Rudloff@Student.HTW-Berlin.de>
2025-12-10 22:34:38 +0100

    mache description not null

--- todo-app-backend/src/main/java/de/htw/berlin/todo_app_backend/domain/ToDoEntry.java
+++ todo-app-backend/src/main/java/de/htw/berlin/todo_app_backend/domain/ToDoEntry.java
@@ -4,9 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Data
@@ -16,7 +16,8 @@ public class ToDoEntry {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @NotNull
+    private String title;
     private String description;
     private boolean done;
     private String dueDate;
